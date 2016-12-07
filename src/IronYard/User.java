747c5cfb85af1e640ccd6d2f4935d49f45e10fec
Matthew Scofield <matthewgscofield@outlook.com>
2016-12-07 12:02:30 -0600

    Initial commit

---
+++ src/IronYard/User.java
@@ -0,0 +1,12 @@
+package IronYard;
+
+/**
+ * Created by scofieldservices on 12/7/16.
+ */
+public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
